c1787621c13fb77da224e74f7476b6aad38f41a8
John <jjkiely@gmail.com>
2015-01-03 23:32:30 +0000

    attempted to define an emacs function

---
+++ emacs.d/init.el
@@ -0,0 +1,15 @@
+;; (defun comment-or-uncomment-region-or-line ()
+;;     "Comments or uncomments the region or the current line if there's no active region."
+;;     (interactive)
+;;     (let (beg end)
+;;         (if (region-active-p)
+;;             (setq beg (region-beginning) end (region-end))
+;; 	  (setq beg (line-beginning-position) end (line-end-position)))
+;;         (comment-or-uncomment-region beg end)))
+
+
+;;(define-key c-mode-base-map (kbd "C-/") 'comment-or-uncomment-line-or-region) 
+
+(global-set-key [M-mouse-1] 'mouse-set-point)
+
+(global-set-key (kbd "C-;") 'comment-region)
