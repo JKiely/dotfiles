81050c9aceec706cd521e1cff2baa6ee73de8881
John <jjkiely@gmail.com>
2015-01-05 17:04:52 +0000

    cleaning up

--- install.sh
+++ install.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
 
 dir=~/dotfiles
 olddir=~/dotfiles_old             
 files="bash_profile bashrc gitconfig gitignore_global emacs.d"
 
+mkdir $olddir
 
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
-
 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
+    echo "Symlinking $file to home ~."
     ln -s $dir/$file ~/.$file
+    echo "Done"
 done
 
