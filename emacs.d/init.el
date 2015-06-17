08c7729ee290360fc83fc62f7058c2573c20ee47
John <jjkiely@gmail.com>
2015-06-17 16:43:15 +0100

    removed unused fuctions

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,15 +1,3 @@
-;; (defun comment-or-uncomment-region-or-line ()
-;;     "Comments or uncomments the region or the current line if there's no active region."
-;;     (interactive)
-;;     (let (beg end)
-;;         (if (region-active-p)
-;;             (setq beg (region-beginning) end (region-end))
-;; 	  (setq beg (line-beginning-position) end (line-end-position)))
-;;         (comment-or-uncomment-region beg end)))
-
-
-;;(define-key c-mode-base-map (kbd "C-/") 'comment-or-uncomment-line-or-region) 
-
 (add-to-list 'load-path "~/.emacs.d/")
 ;(defvar my-packages '(paredit idle-highlight-mode ido-ubiquitous find-file-in-project magit smex scpaste))
 ;(package-initialize)
